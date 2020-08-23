66de2148858eeaaf871c4e66aeecce8bdfdcfb0b
zzhalex <zzh001xx@hotmail.com>
2020-08-23 14:50:47 -0700

    user api

--- routes/users.js
+++ routes/users.js
@@ -1,50 +1,66 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const db = require('../db/dbconnection');
-const User = require('../models/user');
-const Sequelize = require('sequelize');
+const db = require("../db/dbconnection");
+const User = require("../models/user");
+const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  User.findAll()
-    .then(data=>{
-    	res.send(data)
-    })
-    .catch(err => res.send(err))});
 
-router.post('/add',function(req,res,next){
+const { generateJWT, decodeJWT } = require("../function/tokenCheck");
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  User.findAll()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => res.send(err));
+});
+
+router.post("/add", function (req, res, next) {
   let { username, firstname, lastname, email, password } = req.body;
   let errors = [];
 
   // Validate Fields
-  if(!username) {
-    errors.push({ text: 'Please add username' });
+  if (!username) {
+    errors.push({ text: "Please add username" });
   }
-  if(!firstname) {
-    errors.push({ text: 'Please add firstname' });
+  if (!firstname) {
+    errors.push({ text: "Please add firstname" });
   }
-  if(!lastname) {
-    errors.push({ text: 'Please add lastname' });
+  if (!lastname) {
+    errors.push({ text: "Please add lastname" });
   }
-  if(!email) {
-    errors.push({ text: 'Please add email' });
+  if (!email) {
+    errors.push({ text: "Please add email" });
   }
-  if(!password) {
-    errors.push({ text: 'Please add password' });
+  if (!password) {
+    errors.push({ text: "Please add password" });
   }
 
   User.create({
-  	username,
-  	firstname,
-  	lastname,
-  	email,
-  	password
+    username,
+    firstname,
+    lastname,
+    email,
+    password,
   })
-  	.then(() => res.send('Done'))
-  	.catch(err => res.send(err))
+    .then(() => res.send("Done"))
+    .catch((err) => res.send(err));
+});
 
-
+router.get("/info", function (req, res) {
+  let token = req.headers.authorization;
+  let user = decodeJWT(token).user;
+  User.findOne({
+    where: {
+      id: user.id,
+    },
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => res.send(err));
 });
 
 module.exports = router;
